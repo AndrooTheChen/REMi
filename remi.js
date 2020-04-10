e0e6849984d728ff5c2ef1533e6f7230e9321fe2
Andrew Chen <achen226@gmail.com>
2020-04-10 11:53:32 -0500

    roll command now sends random monster without any weights yet

--- remi.js
+++ remi.js
@@ -2,6 +2,25 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
 
+const readline = require('readline');
+const fs = require('fs');
+
+const mon_db = [];
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream(`${process.env.WINHOME}Androo/bots/remy/rem_urls.txt`)
+});
+
+readInterface.on('line', function(line) {
+    let newline = line.split('@')
+    let mon = {
+        name: newline[1],
+        url: newline[0]
+    }
+    mon_db.push(mon);
+    // console.log(`${mon.name}, ${mon.url}`);
+});
+
 // output console log when bot is logged in
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -20,6 +39,10 @@ client.on('message', msg => {
     switch(cmd) {
         case 'ping':
             msg.reply("pong!");
+        break;
+        case 'roll':
+            roll = mon_db[Math.floor(Math.random() * mon_db.length)];
+            msg.reply(`You rolled ${roll.name}!`, {files:[roll.url]});
         break;
     }
 });
